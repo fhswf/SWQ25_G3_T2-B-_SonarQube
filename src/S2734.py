93a6f9f1c05b262cc4f990c04914ec196ec04638
Ayphyer <mail.an.addy@gmx.de>
2025-12-06 11:26:49 +0100

    Fix compliance issue in search_first_number_without_break
    Removed noncompliant else clause from search function.

--- src/S2734.py
+++ src/S2734.py
@@ -16,5 +16,4 @@ def search_first_number_without_break(elements: List[str]):
     for elt in elements:
         if elt.isnumeric():
             return elt
-    else:  # Noncompliant. This will be executed every time
         raise ValueError("List does not contain any number")
